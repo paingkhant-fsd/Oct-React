6a66ad9d9ee1fe39d176f808de318663bcf9890a
paingkhant-fsd <roseeye26@gmail.com>
2025-10-26 19:05:01 +0630

    add useMemo

--- hello-react/src/AppProvider.jsx
+++ hello-react/src/AppProvider.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useMemo, useState } from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 
@@ -9,11 +9,14 @@ const AppContext = createContext();
 export default function AppProvider() {
     const [mode, setMode] = useState("dark");
 
-    const theme = createTheme({
-        palette: {
-            mode: mode,
-        },
-    });
+    const theme = useMemo(() => {
+        console.log("running create theme function");
+        return createTheme({
+            palette: {
+                mode,
+            },
+        });
+    }, [mode]);
 
     return (
         <AppContext.Provider value={{ mode, setMode }}>
